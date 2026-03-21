7d7527b4b375a886d8bfaf7a781a4ebf1655f9d7
jehysonh-rgb <jehysonh@gmail.com>
2026-03-20 21:56:13 -0400

    Update anim.js

--- anim.js
+++ anim.js
@@ -1,10 +1,10 @@
 // Sincronizar las letras con la canción
-var audio = document.querySelector("audio");
+
 var lyrics = document.querySelector("#lyrics");
 
 // Array de objetos que contiene cada línea y su tiempo de aparición en segundos
 var lyricsData = [
-  { text: "No se si esto es amor", time: 0,3 }
+  { text: "No se si esto es amor", time: 0,3 },
   { text: "Rio mirando al phone", time: 0.,7},
   { text: "Nena te envidia el sol", time: 2},
   { text: "Por que brillas como el", time: 4 },
